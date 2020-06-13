aa6b8c5c934520f63a7e4cf439ef939938d89d70
Kun <kun.register@gmail.com>
2020-06-14 05:29:36 +0800

    Add bookInstance.js

--- models/bookInstance.js
+++ models/bookInstance.js
@@ -5,7 +5,7 @@ var Schema = mongoose.Schema
 var BookInstanceSchema = new Schema({
     book: { type: Schema.Types.ObjectId, ref: 'Book', required: true },
     imprint: { type: String, required: true },
-    status: { type: String, required: true, enum: ['Available', 'Maintenance', 'Loaned', 'Reserved'] },
+    status: { type: String, required: true, enum: ['Available', 'Maintenance', 'Loaned', 'Reserved'], default: 'Maintenance' },
     due_back: { type: Date, default: Date.now }
 })
 
