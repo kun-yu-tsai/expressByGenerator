510ed06e6d9c3bffde6dfd513e0b2427238ad6ba
Kun <kun.register@gmail.com>
2020-06-15 14:06:00 +0800

    render index.js response in controller function

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -1,7 +1,8 @@
 var Book = require('../models/book');
 
 exports.index = function(req, res) {
-    res.send('NOT IMPLEMENTED: Site Home Page');
+    // res.send('NOT IMPLEMENTED: Site Home Page');
+    res.render('index',{title: "TTTitle"})
 };
 
 // Display list of all books.
