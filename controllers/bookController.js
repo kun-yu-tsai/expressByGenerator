9c0f916e559ee5b26749af15432d5030d6e8175d
Kun <kun.register@gmail.com>
2020-06-15 20:14:28 +0800

    1. Read data from db in bookController.js 2. apply the result data to index.js 3. show counts in index.js by the pug literal

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -1,46 +1,69 @@
+var async = require('async')
 var Book = require('../models/book');
+var BookInstance = require('../models/bookinstance')
+var Author = require('../models/author')
 
-exports.index = function(req, res) {
-    // res.send('NOT IMPLEMENTED: Site Home Page');
-    res.render('index',{title: "TTTitle"})
+exports.index = function (req, res) {
+    async.parallel(
+        {
+            book_count: function (callback) {
+                Book.countDocuments(callback)
+                // in countDocuments(), it will execute "callback(null, counts)". This is kind of a heartful design from mongoose.
+                // It takes care those tedious things for us.
+            },
+            book_instance_count: function (callback) {
+                BookInstance.countDocuments(callback)
+            },
+            book_instance_available_count: function (callback) { // this is going to be an async function
+                BookInstance.countDocuments({ status: 'Available' }, callback)
+            },
+            author_count: function(callback){
+                Author.countDocuments(callback)
+            },
+
+        },
+        function (err, results) {
+            res.render('index', { title: "TTTitle", data: results })
+        }
+    )
 };
 
 // Display list of all books.
-exports.book_list = function(req, res) {
+exports.book_list = function (req, res) {
     res.send('NOT IMPLEMENTED: Book list');
 };
 
 // Display detail page for a specific book.
-exports.book_detail = function(req, res) {
+exports.book_detail = function (req, res) {
     res.send('NOT IMPLEMENTED: Book detail: ' + req.params.id);
 };
 
 // Display book create form on GET.
-exports.book_create_get = function(req, res) {
+exports.book_create_get = function (req, res) {
     res.send('NOT IMPLEMENTED: Book create GET');
 };
 
 // Handle book create on POST.
-exports.book_create_post = function(req, res) {
+exports.book_create_post = function (req, res) {
     res.send('NOT IMPLEMENTED: Book create POST');
 };
 
 // Display book delete form on GET.
-exports.book_delete_get = function(req, res) {
+exports.book_delete_get = function (req, res) {
     res.send('NOT IMPLEMENTED: Book delete GET');
 };
 
 // Handle book delete on POST.
-exports.book_delete_post = function(req, res) {
+exports.book_delete_post = function (req, res) {
     res.send('NOT IMPLEMENTED: Book delete POST');
 };
 
 // Display book update form on GET.
-exports.book_update_get = function(req, res) {
+exports.book_update_get = function (req, res) {
     res.send('NOT IMPLEMENTED: Book update GET');
 };
 
 // Handle book update on POST.
-exports.book_update_post = function(req, res) {
+exports.book_update_post = function (req, res) {
     res.send('NOT IMPLEMENTED: Book update POST');
 };
